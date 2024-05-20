031308aea83316ecc997114e623d4d4193056915
bdsahiner <bdsahiner@gmail.com>
2024-05-20 10:00:48 +0300

    data types updated

--- RegisteredAnimalDatabase.sql
+++ RegisteredAnimalDatabase.sql
@@ -1,51 +1,56 @@
 CREATE TABLE country (
-country_id number(3) PRIMARY KEY,
-country_name varchar(20) NOT NULL );
+    country_id NUMBER(3) PRIMARY KEY,
+    country_name VARCHAR2(20) NOT NULL
+);
 
 CREATE TABLE owners (
-tc_no number(11) PRIMARY KEY,
-owner_name varchar(25) NOT NULL,
-owner_surname varchar(25) NOT NULL,
-owner_phone number(11) UNIQUE,
-owner_mail varchar(50),
-owner_gender varchar(2),
-owner_birthdate DATE,
-country_id number(3),
-CONSTRAINT owner_country_FK FOREIGN KEY (country_id) 
-REFERENCES country (country_id) ENABLE );
+    tc_no VARCHAR2(11) PRIMARY KEY,
+    owner_name VARCHAR2(25) NOT NULL,
+    owner_surname VARCHAR2(25) NOT NULL,
+    owner_phone VARCHAR2(15) UNIQUE,
+    owner_mail VARCHAR2(50),
+    owner_gender CHAR(1),
+    owner_birthdate DATE,
+    country_id NUMBER(3),
+    CONSTRAINT owner_country_FK FOREIGN KEY (country_id) 
+        REFERENCES country (country_id)
+);
 
 CREATE TABLE passports (
-passport_no char(7) PRIMARY KEY,
-passport_type char(1),
-passport_validdate DATE,
-passport_issueddate DATE,
-country_id number(3),
-tc_no number(11),
-CONSTRAINT passport_country_FK FOREIGN KEY (country_id) 
-REFERENCES country (country_id) ENABLE,
-CONSTRAINT passport_owner_FK FOREIGN KEY (tc_no) 
-REFERENCES owners (tc_no) ENABLE );
+    passport_no CHAR(7) PRIMARY KEY,
+    passport_type CHAR(1),
+    passport_validdate DATE,
+    passport_issueddate DATE,
+    country_id NUMBER(3),
+    tc_no VARCHAR2(11),
+    CONSTRAINT passport_country_FK FOREIGN KEY (country_id) 
+        REFERENCES country (country_id),
+    CONSTRAINT passport_owner_FK FOREIGN KEY (tc_no) 
+        REFERENCES owners (tc_no)
+);
 
 CREATE TABLE animal (
-animal_id number(3) PRIMARY KEY,
-animal_name varchar(25),
-animal_age number(3),
-animal_type varchar(15),
-animal_species varchar (25),
-animal_gender varchar(10),
-animal_color varchar(15),
-animal_birthdate DATE,
-tc_no number(11),
-CONSTRAINT animal_owner_FK FOREIGN KEY (tc_no) 
-REFERENCES owners (tc_no) ENABLE );
+    animal_id NUMBER(3) PRIMARY KEY,
+    animal_name VARCHAR2(25),
+    animal_age NUMBER(3),
+    animal_type VARCHAR2(15),
+    animal_species VARCHAR2(25),
+    animal_gender VARCHAR2(10),
+    animal_color VARCHAR2(15),
+    animal_birthdate DATE,
+    tc_no VARCHAR2(11),
+    CONSTRAINT animal_owner_FK FOREIGN KEY (tc_no) 
+        REFERENCES owners (tc_no)
+);
 
 CREATE TABLE chip_info (
-chip_no number(6) PRIMARY KEY,
-chip_status char(1) NOT NULL,
-chip_date DATE,
-passport_no char(7),
-animal_id number(3),
-CONSTRAINT chip_passport_FK FOREIGN KEY (passport_no) 
-REFERENCES passports	 (passport_no) ENABLE,
-CONSTRAINT chip_animal_FK FOREIGN KEY (animal_id) 
-REFERENCES animal	 (animal_id) ENABLE );
+    chip_no NUMBER(6) PRIMARY KEY,
+    chip_status CHAR(1) NOT NULL,
+    chip_date DATE,
+    passport_no CHAR(7),
+    animal_id NUMBER(3),
+    CONSTRAINT chip_passport_FK FOREIGN KEY (passport_no) 
+        REFERENCES passports (passport_no),
+    CONSTRAINT chip_animal_FK FOREIGN KEY (animal_id) 
+        REFERENCES animal (animal_id)
+);
